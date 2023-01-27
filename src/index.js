4683882aaf39ba5baaf9a9098342a90f93f4308a
mrdraushtam <mrdraushtam@mail.ru>
2023-01-27 18:50:48 +0200

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let otherN = -n;
+    if (n >= 0) {
+        return Number([...n.toString()].reverse().join(""))
+    } else if (n <= 0) {
+        return Number([...otherN.toString()].reverse().join(""))
+    }
+   
 }
